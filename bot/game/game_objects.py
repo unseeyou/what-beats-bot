50a3cd70d3889f11fb09bcb862382dbffac6552d
unseeyou <unseeyoubestmod@gmail.com>
2024-07-27 20:09:20 +1000

    feat: more work on custom game objects
    Signed-off-by: unseeyou <unseeyoubestmod@gmail.com>

--- bot/game/game_objects.py
+++ bot/game/game_objects.py
@@ -1,7 +1,7 @@
 from typing import Literal
 
 import g4f
-from discord import Embed, ui
+from discord import Embed, Interaction, ui
 
 
 class GameEmbed(Embed):
@@ -52,14 +52,45 @@ class GameEmbed(Embed):
         return output
 
 
+class InputModal(ui.Modal):
+    def __init__(self, item: str) -> None:
+        super().__init__(title=f"What beats {item}?")
+
+    answer = ui.TextInput(
+        label="Answer",
+        placeholder="...",
+        style=ui.TextInputStyle.short,
+        required=True,
+        max_length=30,
+    )
+
+    async def on_submit(self, interaction: Interaction) -> None:
+        origin = await interaction.original_response()
+        await origin.edit(view=GameView("explain", self.answer.value))
+
+
+class InputButton(ui.Button):
+    def __init__(self) -> None:
+        super().__init__(style=ui.ButtonStyle.primary, label="Click to answer")
+
+    async def callback(self, interaction: Interaction) -> None:
+        view: GameView = self.view
+        item = view.item
+        await interaction.response.send_modal(
+            InputModal(item),
+        )
+
+
 class GameView(ui.View):
-    def __init__(self, state: Literal["ask", "lose", "explain"]) -> None:
+    def __init__(self, state: Literal["ask", "lose", "explain"], item: str) -> None:
         super().__init__()
         self._state = state
+        self.item = item
         match self._state:
             case "ask":
-                pass
+                self.clear_items()
+                self.add_item(InputButton())
             case "lose":
-                pass
+                self.clear_items()
             case "explain":
-                pass
+                self.clear_items()
